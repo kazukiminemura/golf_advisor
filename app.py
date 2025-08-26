130c45de8e81baa4a03f931a7a13c5930c784281
kazuki minemura <kazuki.minemura@gmail.com>
2025-08-26 21:56:04 +0900

    Serve comparison videos on web page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_from_directory
 
 app = Flask(__name__)
 messages = []  # 簡易的にメッセージをメモリに保持
@@ -17,6 +17,12 @@ def message_handler():
         return "", 204  # 成功時は空でOK
     else:
         return jsonify(messages)
+
+
+@app.route("/videos/<path:filename>")
+def serve_video(filename):
+    """Serve video files from the data directory."""
+    return send_from_directory("data", filename)
 
 
 if __name__ == "__main__":
